3b0a5c43f54118a42f09172a6c9710531de4b755
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2019-10-13 22:53:28 +0900

    edited JS

--- script.js
+++ script.js
@@ -12,7 +12,38 @@ function showScore(){
 **************/
 
 function answerQuiz1(){
-//問題1の処理をここに記述
+    //問題1の処理をここに記述
+    //問１の正解か不正解かを判断する機能＋もし正解だったらスコアを1点ずつプラスする機能
+    const quiz_1 = document.getElementById('quiz-1');
+    if (quiz_1.answer.value === 'a') {
+        //回答aだったら返す反応
+        score = score + 0;
+        //選択したボタンの判定
+        console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+        //回答結果
+        console.log('不正解です');
+        //現在のスコアを表示
+        console.log('現在のトータルスコア：' + score);
+    } else if (quiz_1.answer.value === 'b') {
+        score = score + 1;
+        //選択したボタンの判定
+        console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+        //回答結果
+        console.log('正解です');
+        //現在のスコアを表示
+        console.log('現在のトータルスコア：' + score);
+    } else if (quiz_1.answer.value === 'c') {
+        score= score + 0;
+        //選択したボタンの判定
+        console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+        //回答結果
+        console.log('不正解です');
+        //現在のスコアを表示
+        console.log('現在のトータルスコア：' + score);
+    } else {
+        //アラートで問１の回答をしてください
+        alert('問1の回答をしてください');
+    }
 }
 
 function answerQuiz2(){
